da9cb215b02ec2be1ce84075e8f2d56f7db98774
Nailton Brito Junior <nailton.brito.2003@gmail.com>
2024-02-29 14:12:16 -0300

    Commit dia 1

---
+++ app.js
@@ -0,0 +1,22 @@
+const key = "96139a5736e08e70d872dc84f804c4f3"
+
+function dadosOnInterface(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".imagemWeather").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".imagemWeather").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade) { 
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+
+    dadosOnInterface(dados)
+}
+
+function clickonbutton() { 
+    const cidade = document.querySelector(".cidadeBusca").value
+
+    buscarCidade(cidade)
+}
